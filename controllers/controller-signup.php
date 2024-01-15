9d330fda50d7fb42b576571d82d2e1bc3a310e72
AnousoneM <anousone.mounivongs@gmail.com>
2024-01-15 16:13:08 +0100

    mise à jour

--- controllers/controller-signup.php
+++ controllers/controller-signup.php
@@ -1,25 +1,119 @@
 <?php
 
-// regex du nom
-$regexName = '/^[a-zA-Z]+$/';
+require_once '../config.php';
 
 var_dump($_POST);
 
+// regex du nom
+$regexName = '/^[a-zA-Z]+$/';
+$regexPseudo = '/^[a-zA-Z_\-\d]+$/';
+
+
 // Je déclenche mes tests uniquement lors d'un POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
     // je créé un tablau d'erreurs vide
     $errors = [];
 
-    // Verification du nom
+    // Vérification du nom
     if (empty($_POST['lastname'])) {
         $errors['lastname'] = 'Veuillez saisir un nom';
     } elseif (!preg_match($regexName, $_POST['lastname'])) {
         $errors['lastname'] = 'Caractères non valides';
     }
 
-    var_dump($errors);
-}
+    // Vérification du prénom
+    if (empty($_POST['firstname'])) {
+        $errors['firstname'] = 'Veuillez saisir un prénom';
+    } elseif (!preg_match($regexName, $_POST['lastname'])) {
+        $errors['firstname'] = 'Caractères non valides';
+    }
 
+    // Vérification du pseudo
+    if (empty($_POST['pseudo'])) {
+        $errors['pseudo'] = 'Veuillez saisir un pseudo';
+    } elseif (!preg_match($regexPseudo, $_POST['pseudo'])) {
+        $errors['pseudo'] = 'Caractères non valides';
+    }
+
+    // Vérification du mail
+    if (empty($_POST['email'])) {
+        $errors['email'] = 'Veuillez saisir un courriel';
+    } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'mail non valide';
+    }
+
+    // Vérification de la date de naissance
+    if (empty($_POST['birthdate'])) {
+        $errors['birthdate'] = 'Veuillez saisir une date de naissance';
+    }
+
+    // Vérification du select de l'entreprise
+    if (!isset($_POST['enterprise'])) {
+        $errors['enterprise'] = 'Veuillez selectionner une entreprise';
+    }
+
+    // Vérification de la case des CGU
+    if (!isset($_POST['cgu'])) {
+        $errors['cgu'] = 'Veuillez valider les CGU';
+    }
+
+    // Vérification du password 
+    if (empty($_POST['password'])) {
+        $errors['password'] = 'Veuillez saisir un mot de passe';
+    }
+
+    // Vérification du password 
+    if (empty($_POST['confirmPassword'])) {
+        $errors['confirmPassword'] = 'Veuillez valider votre mot de passe';
+    } else if (!empty($_POST['password'])) {
+        if ($_POST['password'] !== $_POST['confirmPassword']) {
+            $errors['confirmPassword'] = 'Les mots de passe ne sont pas identique';
+        }
+    }
+
+    // Si il n'y a pas d'erreurs dans notre formulaire, nous inscrire l'utilisateur
+    if (empty($errors)) {
+
+        try {
+
+            // Création d'un objet $db selon la classe PDO
+            $db = new PDO("mysql:host=localhost;dbname=" . DBNAME, DBUSERNAME, DBPASSWORD);
+
+            // stockage de ma requete dans une variable
+            $sql = "INSERT INTO `utilisateur` (`nom_participant`,`prenom_participant`, `pseudo_participant`, `naissance_participant`, `mail_participant`, `mdp_participant`, `id_entreprise`, `valide_participant` ) VALUES (:lastname, :firstname, :pseudo, :birthdate, :email, :mdp_participant, :id_enterprise, :valide_participant);";
+
+            // je prepare ma requête pour éviter les injections SQL
+            $query = $db->prepare($sql);
+
+            // on recupère les valeurs des post et nous appliquons un HTMLSPECIALCHARS
+            $lastname = htmlspecialchars($_POST['lastname']);
+            $firstname = htmlspecialchars($_POST['firstname']);
+            $pseudo = htmlspecialchars($_POST['pseudo']);
+            $birthdate = $_POST['birthdate'];
+            $email = $_POST['email'];
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $id_enterprise = $_POST['enterprise'];
+            
+            // on relie les valeurs à nos marqueurs nominatifs à l'aide d'un bindValue
+            $query->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+            $query->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+            $query->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+            $query->bindValue(':birthdate', $birthdate, PDO::PARAM_STR);
+            $query->bindValue(':email', $email, PDO::PARAM_STR);
+            $query->bindValue(':mdp_participant', $password, PDO::PARAM_STR);
+            $query->bindValue(':id_enterprise', $id_enterprise, PDO::PARAM_STR);
+            $query->bindValue(':valide_participant', 1, PDO::PARAM_INT);
+
+            // on execute la requête
+            $query->execute();
+
+        } catch (PDOException $e) {
+            echo 'Erreur : ' . $e->getMessage();
+            die();
+        }
+    }
+}
 
 ?>
 
--- views/view-signup.php
+++ views/view-signup.php
@@ -5,9 +5,9 @@
 
 <div class="container my-5 col-8 px-5 py-4 shadow rounded bg-white">
 
-    <form action="" method="POST">
+    <form action="" method="POST" novalidate>
         <div class="my-3">
-            <label for="lastname">Nom</label>
+            <label class="me-1" for="lastname">Nom</label>
             <!-- Je fais une value pour conserver les données du formulaire si erreur -->
             <input type="text" name="lastname" id="lastname" placeholder="ex. DOE" value="<?= $_POST['lastname'] ?? '' ?>">
             <!-- Message d'erreur si non OK -->
@@ -15,51 +15,53 @@
         </div>
 
         <div class="my-3">
-            <label for="firstname">Prénom</label>
-            <input type="text" name="firstname" id="firstname" placeholder="ex. John">
+            <label class="me-1 for="firstname">Prénom</label>
+            <input type="text" name="firstname" id="firstname" placeholder="ex. John" value="<?= $_POST['firstname'] ?? '' ?>">
             <span class="input-warning ms-2"><?= $errors['firstname'] ?? '' ?></span>
         </div>
 
         <div class="my-3">
-            <label for="pseudo">Pseudo</label>
-            <input type="text" name="pseudo" id="pseudo" placeholder="ex. John">
-            <span class="input-warning ms-2"></span>
+            <label class="me-1 for="pseudo">Pseudo</label>
+            <input type="text" name="pseudo" id="pseudo" placeholder="ex. Roronoa76" value="<?= $_POST['pseudo'] ?? '' ?>">
+            <span class="input-warning ms-2"><?= $errors['pseudo'] ?? '' ?></span>
         </div>
 
         <div class="my-3">
-            <label for="birthdate">Date de naissance</label>
-            <input type="date" name="birthdate" id="birthdate">
-            <span class="input-warning ms-2"></span>
+            <label class="me-1 for="birthdate">Date de naissance</label>
+            <input type="date" name="birthdate" id="birthdate" value="<?= $_POST['birthdate'] ?? '' ?>">
+            <span class="input-warning ms-2"><?= $errors['birthdate'] ?? '' ?></span>
         </div>
 
         <div class="my-3">
-            <label for="">Courriel</label>
+            <label class="me-1 for="">Courriel</label>
             <input type="email" name="email" id="email" placeholder="ex. mon-mail@mail.fr">
-            <span class="input-warning ms-2"></span>
+            <span class="input-warning ms-2"><?= $errors['email'] ?? '' ?></span>
         </div>
 
+        <label class="me-1 for="enterprise">Entreprise</label>
         <select name="enterprise" id="enterprise">
             <option value="" selected disabled>-- Veuillez sélectionner votre entreprise --</option>
-            <option value="1">Bad Company</option>
-            <option value="2">Afpa</option>
+            <option value="1" <?= isset($_POST['enterprise']) && $_POST['enterprise'] == 1 ? 'selected' : '' ?> >Bad Company</option>
+            <option value="2" <?= isset($_POST['enterprise']) && $_POST['enterprise'] == 2 ? 'selected' : '' ?> >Afpa</option>
         </select>
+        <span class="input-warning ms-2"><?= $errors['enterprise'] ?? '' ?></span>
 
         <div class="my-3">
-            <label for="password">Mot de passe</label>
+            <label class="me-1 for="password">Mot de passe</label>
             <input type="password" name="password" id="password">
-            <span class="input-warning ms-2"></span>
+            <span class="input-warning ms-2"><?= $errors['password'] ?? '' ?></span>
         </div>
 
         <div class="my-3">
-            <label for="confirmPassword">Confirmation du mot de passe</label>
+            <label class="me-1 for="confirmPassword">Confirmation du mot de passe</label>
             <input type="password" name="confirmPassword" id="confirmPassword">
-            <span class="input-warning ms-2"></span>
+            <span class="input-warning ms-2"><?= $errors['confirmPassword'] ?? '' ?></span>
         </div>
 
         <div class="my-4">
             <input name="cgu" id="cgu" type="checkbox">
-            <label for="cgu">J'accepte les CGU</label>
-            <span class="input-warning ms-2"></span>
+            <label class="me-1 for="cgu">J'accepte les CGU</label>
+            <span class="input-warning ms-2"><?= $errors['cgu'] ?? '' ?></span>
         </div>
 
         <button class="mt-3 btn btn-outline-secondary">S'enregistrer</button>
