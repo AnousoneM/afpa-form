ff6063c40d99cdbb34e0254e7d48bb41d1aa92f0
Anousone M. <anousone.mounivongs@gmail.com>
2024-01-19 01:21:04 +0100

    session ok

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// penser à mettre à jour votre config !!!!!
+
+define('DBNAME', 'metro_boulot_dodo');
+define('DBUSERNAME', 'ecoride');
+define('DBPASSWORD', 'ecoride');
+
+define('SHOWPAGE','showpage');
