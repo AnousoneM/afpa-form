e7a8767dc11952cdb4335c0a6c694ae7f8e25c7e
Anousone M. <anousone.mounivongs@gmail.com>
2024-01-21 23:32:40 +0100

    mise à jour erreur version mobile

--- views/view-signup.php
+++ views/view-signup.php
@@ -14,7 +14,7 @@
             <p class="fs-2 text-center text-success"><i class="fa-solid fa-house-chimney"></i><i class="ms-1 fa-solid fa-bicycle"></i><i class="ms-5 fa-solid fa-building"></i></p>
 
             <label class="me-1 fw-bold" for="enterprise">Entreprise</label>
-            <span class="input-warning ms-2"><?= $errors['enterprise'] ?? '' ?></span>
+            <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['enterprise'] ?? '' ?></span>
             <select class="form-control <?= isset($errors['enterprise']) ? 'is-invalid' : '' ?> <?= isset($_POST['enterprise']) && !isset($errors['enterprise']) ? 'is-valid' : '' ?>" name="enterprise" id="enterprise">
                 <option value="" selected disabled>-- Veuillez sélectionner votre entreprise --</option>
                 <option value="1" <?= isset($_POST['enterprise']) && $_POST['enterprise'] == 1 ? 'selected' : '' ?>>Bad Company</option>
@@ -23,7 +23,7 @@
 
             <div class="my-3">
                 <label class="me-1 fw-bold" for="lastname">Nom</label>
-                <span class="input-warning ms-2"><?= $errors['lastname'] ?? '' ?></span>
+                <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['lastname'] ?? '' ?></span>
                 <!-- Je fais une value pour conserver les données du formulaire si erreur -->
                 <input class="form-control <?= isset($errors['lastname']) ? 'is-invalid' : '' ?> <?= isset($_POST['lastname']) && !isset($errors['lastname']) ? 'is-valid' : '' ?>" type="text" name="lastname" id="lastname" placeholder="ex. DOE" value="<?= $_POST['lastname'] ?? '' ?>">
                 <!-- Message d'erreur si non OK -->
@@ -32,40 +32,40 @@
 
             <div class="my-3">
                 <label class="me-1 fw-bold" for="firstname">Prénom</label>
-                <span class="input-warning ms-2"><?= $errors['firstname'] ?? '' ?></span>
+                <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['firstname'] ?? '' ?></span>
                 <input class="form-control <?= isset($errors['lastname']) ? 'is-invalid' : '' ?> <?= isset($_POST['lastname']) && !isset($errors['lastname']) ? 'is-valid' : '' ?>" type="text" name="firstname" id="firstname" placeholder="ex. John" value="<?= $_POST['firstname'] ?? '' ?>">
 
             </div>
 
             <div class="my-3">
                 <label class="me-1 fw-bold" for="pseudo">Pseudo</label>
-                <span class="input-warning ms-2"><?= $errors['pseudo'] ?? '' ?></span>
+                <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['pseudo'] ?? '' ?></span>
                 <input class="form-control <?= isset($errors['pseudo']) ? 'is-invalid' : '' ?> <?= isset($_POST['pseudo']) && !isset($errors['pseudo']) ? 'is-valid' : '' ?>" type="text" name="pseudo" id="pseudo" placeholder="ex. Roronoa76" value="<?= $_POST['pseudo'] ?? '' ?>">
 
             </div>
 
             <div class="my-3">
                 <label class="me-1 fw-bold" for="birthdate">Date de naissance</label>
-                <span class="input-warning ms-2"><?= $errors['birthdate'] ?? '' ?></span>
+                <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['birthdate'] ?? '' ?></span>
                 <input class="form-control <?= isset($errors['birthdate']) ? 'is-invalid' : '' ?> <?= isset($_POST['birthdate']) && !isset($errors['birthdate']) ? 'is-valid' : '' ?>" type="date" name="birthdate" id="birthdate" value="<?= $_POST['birthdate'] ?? '' ?>">
 
             </div>
 
             <div class="my-3">
                 <label class="me-1 fw-bold" for="email">Courriel</label>
-                <span class=" input-warning ms-2"><?= $errors['email'] ?? '' ?></span>
+                <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['email'] ?? '' ?></span>
                 <input class="form-control <?= isset($errors['email']) ? 'is-invalid' : '' ?> <?= isset($_POST['email']) && !isset($errors['email']) ? 'is-valid' : '' ?>" type="email" name="email" id="email" placeholder="ex. mon-mail@mail.fr" value="<?= $_POST['email'] ?? '' ?>">
             </div>
 
             <div class="my-3">
                 <label class="me-1 fw-bold" for="password">Mot de passe</label>
-                <span class="input-warning ms-2"><?= $errors['password'] ?? '' ?></span>
+                <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['password'] ?? '' ?></span>
                 <input class="form-control <?= isset($errors['password']) ? 'is-invalid' : '' ?> <?= isset($_POST['password']) && !isset($errors['password']) ? 'is-valid' : '' ?>" type="password" name="password" id="password">
             </div>
 
             <div class="my-3">
                 <label class="me-1 fw-bold" for="confirmPassword">Confirmation du mot de passe</label>
-                <span class="input-warning ms-2"><?= $errors['confirmPassword'] ?? '' ?></span>
+                <span class="d-lg-inline d-none input-warning ms-2"><?= $errors['confirmPassword'] ?? '' ?></span>
                 <input class="form-control <?= isset($errors['confirmPassword']) ? 'is-invalid' : '' ?> <?= isset($_POST['confirmPassword']) && !isset($errors['confirmPassword']) ? 'is-valid' : '' ?>" type="password" name="confirmPassword" id="confirmPassword">
             </div>
 
